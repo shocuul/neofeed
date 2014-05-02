ca51fff7782f51c313a6a126ab7b6b349d9d5270
Jose David <josepdvahn@me.com>
2014-05-01 20:30:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neofeed::Application.config.secret_key_base = 'f69095739a1265bae2d8b78aeb98bdd291ac1ff86694fc42699037f891993ac4c9dbde24498f9e50230f415b5cd30dbdc24e54026b2f1cf9bb1849bfb9eacf18'
